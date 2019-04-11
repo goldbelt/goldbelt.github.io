1d7aa1939c30463e46db3302b6986b02c668b06f
akshithasingathi <45468709+akshithasingathi@users.noreply.github.com>
2019-04-10 21:02:50 -0400

    update

--- projects/app.js
+++ projects/app.js
@@ -38,11 +38,11 @@ function start(username, data){
         var status = userData[project]["status"];
         var btn="";
         if(status == "DONE"){
-            btn=`<button type="button" class="btn btn-success btn-block" onclick="btn('`+project+`')">Revisit</button>`;
+            btn=`<button type="button" class="btn btn-success btn-block" style="color: rgb(230, 252, 172); background-color: rgb(255, 196, 0)" onclick="btn('`+project+`')">Revisit</button>`;
         }else if(status == "IN PROGRESS"){
-            btn=`<button type="button" class="btn btn-primary btn-block" style="color: rgb(230, 252, 172)" onclick="btn('`+project+`')">Continue</button>`;
+            btn=`<button type="button" class="btn btn-primary btn-block" style="color: rgb(230, 252, 172); background-color: rgb(255, 196, 0)" onclick="btn('`+project+`')">Continue</button>`;
         }else{
-            btn=`<button type="button" class="btn btn-info btn-block" onclick="btn('`+project+`')">Start</button>`;
+            btn=`<button type="button" class="btn btn-info btn-block" style="color: rgb(230, 252, 172); background-color: rgb(255, 196, 0)" onclick="btn('`+project+`')">Start</button>`;
         }
         tr.innerHTML = `<tr>
             <th scope="row" style="color:  rgb(230, 252, 172)">`+project+`</th>
