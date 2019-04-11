bca3293f75847f948c957c7f5e1e31215cb13179
KihtrakRaknas <karthikandsankar@gmail.com>
2019-04-10 20:45:23 -0400

    added support for grading screen

--- dojo/app.js
+++ dojo/app.js
@@ -75,6 +75,7 @@ function start(username, data){
 
 function submit(){
     if(usernameGlobal&&projectNameFromParams){
+        new QRCode(document.getElementById("qrcode"), "http://goldbelt.github.io/sensei/grading/index.html?username=+"+username+"&project="+projectNameFromParams);
         $('#senseiCheck').modal('show');
         firebase.database().ref("Accounts/"+usernameGlobal+"/"+projectNameFromParams).update({
             status: "AWAITING SENSEI APPROVAL",
