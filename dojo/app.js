b1bfb6454f6036203d331ef01a949cbb189d96dd
Karthik Sankar <karthikandsankar@gmail.com>
2019-04-15 14:04:59 -0400

    scratch instructions with links are automatically clickable

--- dojo/app.js
+++ dojo/app.js
@@ -85,7 +85,7 @@ function start(username, data){
     }else if(projectData.scratchInstuctions){
         document.getElementById("scratchDojo").style.display = "block";
 
-document.getElementById("scratchIntructions").innerText = projectData.scratchInstuctions
+	document.getElementById("scratchIntructions").innerText = linkify(projectData.scratchInstuctions);
 
         var input = document.getElementById("scratchInput")
 
@@ -129,6 +129,11 @@ document.getElementById("scratchIntructions").innerText = projectData.scratchIns
 
 
         
+}
+
+function linkify(text) {
+        var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        return text.replace(exp, "<a href='$1' target='_blank'>$1</a>");
 }
 
 function submit(){
