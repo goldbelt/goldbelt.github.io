92ce32ccc0d9231debbfb886dbeb2602fb41dba1
Karthik Sankar <10013096@sbschools.org>
2019-04-15 11:00:01 -0400

    bug fix

--- projects/app.js
+++ projects/app.js
@@ -34,29 +34,31 @@ function start(username, data){
     var projects = Object.keys(userData)
     tableBody.innerHTML = "";
     for(project of projects){
-        var tr = document.createElement("tr")
-        var status = userData[project]["status"];
-        var btn="";
-        var type = "Code"
-        if(userData[project].questions)
-            type = "Quiz"
-        else if(userData[project].scratchInstuctions)
-            type = "Scratch Project"
-        if(status == "DONE"){
-            btn=`<button type="button" class="btn btn-success btn-block" style="color: rgb(255, 239, 15); background-color: black" onclick="btn('`+project+`')">Revisit</button>`;
-        }else if(status == "IN PROGRESS"){
-            btn=`<button type="button" class="btn btn-primary btn-block" style="background-color: rgb(255, 45, 45); color: black" onclick="btn('`+project+`')">Continue</button>`;
-        }else{
-            btn=`<button type="button" class="btn btn-info btn-block" style="background-color: rgb(255, 239, 15); color: black" onclick="btn('`+project+`')">Start</button>`;
-        }
+        if(project != "stat"){
+            var tr = document.createElement("tr")
+            var status = userData[project]["status"];
+            var btn="";
+            var type = "Code"
+            if(userData[project].questions)
+                type = "Quiz"
+            else if(userData[project].scratchInstuctions)
+                type = "Scratch Project"
+            if(status == "DONE"){
+                btn=`<button type="button" class="btn btn-success btn-block" style="color: rgb(255, 239, 15); background-color: black" onclick="btn('`+project+`')">Revisit</button>`;
+            }else if(status == "IN PROGRESS"){
+                btn=`<button type="button" class="btn btn-primary btn-block" style="background-color: rgb(255, 45, 45); color: black" onclick="btn('`+project+`')">Continue</button>`;
+            }else{
+                btn=`<button type="button" class="btn btn-info btn-block" style="background-color: rgb(255, 239, 15); color: black" onclick="btn('`+project+`')">Start</button>`;
+            }
 
-        tr.innerHTML = `<tr>
-            <th scope="row" style="color:  rgb(230, 252, 172)">`+project+`</th>
-            <td style="color:  rgb(230, 252, 172)">`+type+`</td>
-            <td style="color:  rgb(230, 252, 172)">`+status+`</td>
-            <td style="color: black">`+btn+`</td>
-         </tr>`
-         tableBody.appendChild(tr)
+            tr.innerHTML = `<tr>
+                <th scope="row" style="color:  rgb(230, 252, 172)">`+project+`</th>
+                <td style="color:  rgb(230, 252, 172)">`+type+`</td>
+                <td style="color:  rgb(230, 252, 172)">`+status+`</td>
+                <td style="color: black">`+btn+`</td>
+            </tr>`
+            tableBody.appendChild(tr)
+        }
     }
 }
 
